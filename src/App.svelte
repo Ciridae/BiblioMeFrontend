3ac9ba2813d541a44a7e0ce5b760b2ee6f424bc1
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-04-20 14:40:20 +0200

    Estructura del proyecto y crear algunos componentes

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { setContext } from "svelte";
+  import { Router } from "svelte-routing";
+  import Nav from "./Nav.svelte";
+  import Contenido from "./Contenido.svelte";
+
+  const URL = {
+    libros: "https://libreriabackend.herokuapp.com/api/libros/",
+    autores: "https://libreriabackend.herokuapp.com/api/autores/",
+  };
+  setContext("URL", URL);
+</script>
+
+<Router>
+  <Nav />
+  <Contenido />
+</Router>
