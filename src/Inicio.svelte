4e0340062893ceb7d4ea8df2183b880d3c857db4
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-05-10 14:31:51 +0200

    Añadir contenido a Inicio

--- src/Inicio.svelte
+++ src/Inicio.svelte
@@ -5,4 +5,24 @@
         La aplicación que ofrece a sus usuarios la posibilidad de reservar
         libros en línea mediante un sencillo proceso en la web.
     </p>
+
+    <div class="row mt-5">
+        <div class="col-md-6 d-flex justify-content-center align-content-center flex-wrap">
+            <ul>
+                <li class="text-left"><b>Reserva fácil:</b> nuestra interfaz de usuario intuitiva y fácil de usar hace que reservar tus libros sea una tarea simple y rápida. Solo selecciona los libros que deseas y ¡listo!</li>
+                <li class="text-left pt-3"><b>Amplia selección de libros:</b> tenemos una amplia variedad de libros para elegir, desde los bestsellers más populares hasta los clásicos atemporales. ¡Encuentra tu próxima gran lectura con nosotros!</li>
+                <li class="text-left pt-3"><b>Recogida sin complicaciones:</b> una vez que hayas reservado tus libros, solo tienes que venir a la biblioteca en la fecha de recogida seleccionada y recogerlos. ¡No hay esperas ni complicaciones!</li>
+              </ul>
+          </div>
+
+          <div class="col-md-6">
+            <video src="/images/video_2_original.mp4" autoplay muted loop class="w-100"></video>
+          </div>
+    </div>
 </div>
+
+<style>
+    li b  {
+        font-size: large;
+    }
+</style>
