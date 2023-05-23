d75246c63840fbab85cacffcc9bee8d2477094d1
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-05-23 13:57:42 +0200

    Cambiar los endpoints de la API, todavía no funciona

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,18 @@
   import Contenido from "./Contenido.svelte";
   import Footer from "./Footer.svelte";
 
-  const URL = {
+/*   const URL = {
     libros: "http://localhost:4343/libros",
     autores: "http://localhost:4343/autores",
     usuarios: "http://localhost:4343/usuarios",
     reservas: "http://localhost:4343/reservas",
+  }; */
+
+  const URL = {
+    libros: "https://bibliome.herokuapp.com/libros",
+    autores: "https://bibliome.herokuapp.com/autores",
+    usuarios: "https://bibliome.herokuapp.com/usuarios",
+    reservas: "https://bibliome.herokuapp.com/reservas",
   };
   setContext("URL", URL);
 </script>
